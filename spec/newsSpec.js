7ad1eed461e9f440d66ec58ad98f960b74ec7cee
Chris Mitchell <chrisjmit@gmail.com>
2017-05-25 13:13:54 +0100

    guardian api added

---
+++ spec/newsSpec.js
@@ -0,0 +1,10 @@
+//example test
+//
+// function testListStoresSingleNote(){
+//   console.log("Test 3: List stores a single note");
+//   var list = new List();
+//   list.addNote("say hello");
+//   assert.isEq(list.getNote(0).getText(), "say hello");
+//   }
+//
+// testListStoresSingleNote();
---
+++ src/news.js
@@ -0,0 +1,5 @@
+const guardian = require('guardian-js');
+
+var apiKey = '739523f6-1d55-4e44-b5c0-c70d5917e378';
+
+let api = new guardian(apiKey, false);
